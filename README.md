39863666332ef7debf039f44378b0bfb357467a8
S <68754950+DrPython3@users.noreply.github.com>
2021-09-19 18:13:28 +0200

    Update with first sample code
    Publishing first sample code (here: basic function for everyday use) and therefor updating the README.md, too.

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 ﻿# Little PY-Helpers
 
-Various Python 3.x scripts for different purposes, e.g. performing common tasks or providing frequently needed functions in other projects and so on. Updated just from time to time.
+Various Python 3.x scripts for different purposes, e.g. performing common tasks or providing frequently needed functions in other projects and so on.<br>
+Updated just from time to time.
 
 Free to use for everyone. But please, do not forget to refer to this repository when using any code provided here.
+
+## Available Content
+
+|No.#|FILE NAME|INFO|LAST UPDATE|
+|----|---------|----|-----------|
+|1|**rm-duplicates.py**|Basic function for removing duplicate lines from a textfile|2021-09-19|
 
 ## Support this Repository
 
@@ -13,5 +20,4 @@ If you like this repo or in case you have found anything helpful here, support m
 
 ### Status
 
-Last Update: 2021-09-18
-
+Last Update: 2021-09-19
---
+++ rm-duplicates.py
@@ -0,0 +1,49 @@
+#!/usr/local/opt/python@3.8/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+**rm-duplicates.py**
+Remove duplicate lines from textfiles.
+
+Part of "Little PY-Helpers" @ GitHub.com
+'''
+
+__author__ = 'DrPython3'
+__date__ = '2021-09-xx'
+__version__ = '1.0'
+__contact__ = 'https://github.com/DrPython3'
+
+# [FUNCTION SAMPLE]:
+def rm_duplicates(targetfile):
+    '''
+    Checks each line in a textfile whether its unique or not.
+
+    :param str targetfile: textfile to check
+    :return: None
+    '''
+    already_existing = set()
+    lines_checked = 0
+    lines_unique = 0
+    try:
+        with open(targetfile, 'r+') as targetcontent:
+            to_check = targetcontent.readlines()
+            targetcontent.seek(0)
+            for line in to_check:
+                lines_checked += 1
+                if line.replace('\n', '') not in already_existing:
+                    targetcontent.write(line.replace('\n', ''))
+                    already_existing.add(line.replace('\n', ''))
+                    lines_unique += 1
+                else:
+                    continue
+            targetcontent.truncate()
+        del already_existing
+        print(f'Lines checked: {str(lines_checked)}')
+        print(f'Unique lines left: {str(lines_unique)}')
+    except:
+        print('Sorry!\nAn error occurred. Check file and try again.')
+    return None
+
+# [TEST THE FUNCTION]:
+testfile = input('Testfile, e.g. testfile.txt: ')
+rm_duplicates(str(testfile))
